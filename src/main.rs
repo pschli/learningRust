e80bfa8d672cb4b39f69e9624e6158fe5ae05bcf
Patrick Schlieker <patrick.schlieker@gmail.com>
2025-12-28 16:47:30 +0100

    RefCell for interior mutability

--- src/main.rs
+++ src/main.rs
@@ -1962,22 +1962,99 @@
 //     let c = Cons(4, Rc::clone(&a));
 // }
 
-enum List {
-    Cons(i32, Rc<List>),
-    Nil,
+// enum List {
+//     Cons(i32, Rc<List>),
+//     Nil,
+// }
+
+// use crate::List::{Cons, Nil};
+// use std::rc::Rc;
+
+// fn main() {
+//     let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+//     println!("count after creating a = {}", Rc::strong_count(&a));
+//     let _b = Cons(3, Rc::clone(&a));
+//     println!("count after creating b = {}", Rc::strong_count(&a));
+//     {
+//         let _c = Cons(4, Rc::clone(&a));
+//         println!("count after creating c = {}", Rc::strong_count(&a));
+//     }
+//     println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+// }
+
+// ****************************************************************************
+// ***** RefCell<T> and interior mutability *****
+
+pub trait Messenger {
+    fn send(&self, msg: &str);
 }
 
-use crate::List::{Cons, Nil};
-use std::rc::Rc;
+pub struct LimitTracker<'a, T: Messenger> {
+    messenger: &'a T,
+    value: usize,
+    max: usize,
+}
 
-fn main() {
-    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
-    println!("count after creating a = {}", Rc::strong_count(&a));
-    let _b = Cons(3, Rc::clone(&a));
-    println!("count after creating b = {}", Rc::strong_count(&a));
-    {
-        let _c = Cons(4, Rc::clone(&a));
-        println!("count after creating c = {}", Rc::strong_count(&a));
+impl<'a, T> LimitTracker<'a, T>
+where
+    T: Messenger,
+{
+    pub fn new(messenger: &'a T, max: usize) -> LimitTracker<'a, T> {
+        LimitTracker {
+            messenger,
+            value: 0,
+            max,
+        }
     }
-    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+
+    pub fn set_value(&mut self, value: usize) {
+        self.value = value;
+
+        let percentage_of_max = self.value as f64 / self.max as f64;
+
+        if percentage_of_max >= 1.0 {
+            self.messenger.send("Error: You are over your quota!");
+        } else if percentage_of_max >= 0.9 {
+            self.messenger
+                .send("Urgent warning: You've used up over 90% of your quota!");
+        } else if percentage_of_max >= 0.75 {
+            self.messenger
+                .send("Warning: You've used up over 75% of your quota!");
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::cell::RefCell;
+
+    struct MockMessenger {
+        sent_messages: RefCell<Vec<String>>, // Vec<String>,
+    }
+
+    impl MockMessenger {
+        fn new() -> MockMessenger {
+            MockMessenger {
+                sent_messages: RefCell::new(vec![]), // vec![],
+            }
+        }
+    }
+
+    impl Messenger for MockMessenger {
+        fn send(&self, message: &str) {
+            // self.sent_messages.push(String::from(message)); This violates borrow as mut rule
+            self.sent_messages.borrow_mut().push(String::from(message)); // this doesn't, but uses unsafe
+        }
+    }
+
+    #[test]
+    fn it_sends_an_over_75_percent_warning_message() {
+        let mock_messenger = MockMessenger::new();
+        let mut limit_tracker = LimitTracker::new(&mock_messenger, 100);
+
+        limit_tracker.set_value(80);
+
+        assert_eq!(mock_messenger.sent_messages.borrow().len(), 1);
+    }
 }
