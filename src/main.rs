2a4b5c544f7ccb8d46f31f6b4bf8932c05913051
Patrick Schlieker <patrick.schlieker@gmail.com>
2025-12-29 13:18:57 +0100

    RefCell

--- src/main.rs
+++ src/main.rs
@@ -1985,76 +1985,109 @@
 // ****************************************************************************
 // ***** RefCell<T> and interior mutability *****
 
-pub trait Messenger {
-    fn send(&self, msg: &str);
-}
+// pub trait Messenger {
+//     fn send(&self, msg: &str);
+// }
 
-pub struct LimitTracker<'a, T: Messenger> {
-    messenger: &'a T,
-    value: usize,
-    max: usize,
-}
+// pub struct LimitTracker<'a, T: Messenger> {
+//     messenger: &'a T,
+//     value: usize,
+//     max: usize,
+// }
 
-impl<'a, T> LimitTracker<'a, T>
-where
-    T: Messenger,
-{
-    pub fn new(messenger: &'a T, max: usize) -> LimitTracker<'a, T> {
-        LimitTracker {
-            messenger,
-            value: 0,
-            max,
-        }
-    }
+// impl<'a, T> LimitTracker<'a, T>
+// where
+//     T: Messenger,
+// {
+//     pub fn new(messenger: &'a T, max: usize) -> LimitTracker<'a, T> {
+//         LimitTracker {
+//             messenger,
+//             value: 0,
+//             max,
+//         }
+//     }
 
-    pub fn set_value(&mut self, value: usize) {
-        self.value = value;
+//     pub fn set_value(&mut self, value: usize) {
+//         self.value = value;
 
-        let percentage_of_max = self.value as f64 / self.max as f64;
+//         let percentage_of_max = self.value as f64 / self.max as f64;
 
-        if percentage_of_max >= 1.0 {
-            self.messenger.send("Error: You are over your quota!");
-        } else if percentage_of_max >= 0.9 {
-            self.messenger
-                .send("Urgent warning: You've used up over 90% of your quota!");
-        } else if percentage_of_max >= 0.75 {
-            self.messenger
-                .send("Warning: You've used up over 75% of your quota!");
-        }
-    }
-}
+//         if percentage_of_max >= 1.0 {
+//             self.messenger.send("Error: You are over your quota!");
+//         } else if percentage_of_max >= 0.9 {
+//             self.messenger
+//                 .send("Urgent warning: You've used up over 90% of your quota!");
+//         } else if percentage_of_max >= 0.75 {
+//             self.messenger
+//                 .send("Warning: You've used up over 75% of your quota!");
+//         }
+//     }
+// }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use std::cell::RefCell;
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//     use std::cell::RefCell;
 
-    struct MockMessenger {
-        sent_messages: RefCell<Vec<String>>, // Vec<String>,
-    }
+//     struct MockMessenger {
+//         sent_messages: RefCell<Vec<String>>, // Vec<String>,
+//     }
 
-    impl MockMessenger {
-        fn new() -> MockMessenger {
-            MockMessenger {
-                sent_messages: RefCell::new(vec![]), // vec![],
-            }
-        }
-    }
+//     impl MockMessenger {
+//         fn new() -> MockMessenger {
+//             MockMessenger {
+//                 sent_messages: RefCell::new(vec![]), // vec![],
+//             }
+//         }
+//     }
 
-    impl Messenger for MockMessenger {
-        fn send(&self, message: &str) {
-            // self.sent_messages.push(String::from(message)); This violates borrow as mut rule
-            self.sent_messages.borrow_mut().push(String::from(message)); // this doesn't, but uses unsafe
-        }
-    }
+//     impl Messenger for MockMessenger {
+//         fn send(&self, message: &str) {
+//             // self.sent_messages.push(String::from(message)); This violates borrow as mut rule
+//             self.sent_messages.borrow_mut().push(String::from(message)); // this doesn't, but uses unsafe
+//         }
+//     }
 
-    #[test]
-    fn it_sends_an_over_75_percent_warning_message() {
-        let mock_messenger = MockMessenger::new();
-        let mut limit_tracker = LimitTracker::new(&mock_messenger, 100);
+//     #[test]
+//     fn it_sends_an_over_75_percent_warning_message() {
+//         let mock_messenger = MockMessenger::new();
+//         let mut limit_tracker = LimitTracker::new(&mock_messenger, 100);
 
-        limit_tracker.set_value(80);
+//         limit_tracker.set_value(80);
 
-        assert_eq!(mock_messenger.sent_messages.borrow().len(), 1);
-    }
-}
+//         assert_eq!(mock_messenger.sent_messages.borrow().len(), 1);
+//     }
+// }
+// When creating immutable and mutable references, we use the & and &mut syntax, respectively.
+// With RefCell<T>, we use the borrow and borrow_mut methods, which are part of the safe API that belongs to RefCell<T>.
+// The borrow method returns the smart pointer type Ref<T>, and borrow_mut returns the smart pointer type RefMut<T>.
+// Both types implement Deref, so we can treat them like regular references.
+// RefCell<T> lets us have many immutable borrows or one mutable borrow at any point in time.
+
+// #[derive(Debug)]
+// enum List {
+//     Cons(Rc<RefCell<i32>>, Rc<List>),
+//     Nil,
+// }
+
+// use crate::List::{Cons, Nil};
+// use std::cell::RefCell;
+// use std::rc::Rc;
+
+// fn main() {
+//     let value = Rc::new(RefCell::new(5));
+
+//     let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+
+//     let b = Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
+//     let c = Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
+
+//     *value.borrow_mut() += 10;
+
+//     println!("a after = {a:?}");
+//     println!("b after = {b:?}");
+//     println!("c after = {c:?}");
+// }
+
+// *******************************************************************************************
+// ***** Memory Leaks through Rc and RefCell *****
